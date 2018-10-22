3bcde4642e56e6e11b15f66987e64c1c0ad00cc6
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-22 23:09:33 +0300

    Setup project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Text Editor
+
+Project: Text Editor
+
+Go to the *src* directory, change *Main.java* and put your *.java* file(s) in it.
---
+++ src/editor/TextEditor.java
@@ -0,0 +1,7 @@
+package editor;
+
+public class TextEditor {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
