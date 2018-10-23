f39129035af9be3f394a75434c6d7365a11804d2
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-23 19:25:25 +0300

    Break TextEditor into 2 classes

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -3,15 +3,9 @@ package editor;
 import javax.swing.*;
 
 public class TextEditor extends JFrame {
-
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setVisible(true);
     }
-
-    public static void main(String[] args) {
-        new TextEditor();
-    }
-
 }
