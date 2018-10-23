2606106680b8b3f6565f2c9e0870045336398c5d
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-23 18:50:49 +0300

    Setup minimal GUI

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -1,7 +1,17 @@
 package editor;
 
-public class TextEditor {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+
+    public TextEditor() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
     }
+
+    public static void main(String[] args) {
+        new TextEditor();
+    }
+
 }
