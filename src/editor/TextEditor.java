aa789e0b3574e6f60aea5b1c03c6e8ce75b66fff
Vladislav <v.shichanov@gmail.com>
2018-11-20 10:33:08 +0300

    Was add text area for simple text editor

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -1,11 +1,41 @@
 package editor;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initTextArea();
+
         setSize(300, 300);
+        setTitle("Text editor v 0.1");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setVisible(true);
+
+    }
+    private void initTextArea() {
+
+        JTextArea textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+
+        createLayout(scrollPane);
+    }
+
+
+    private void createLayout (JComponent scrollPane) {
+        Container pane = getContentPane();
+        GroupLayout groupLayout = new GroupLayout(pane);
+        pane.setLayout(groupLayout);
+
+        groupLayout.setAutoCreateContainerGaps(true);
+        groupLayout.setAutoCreateGaps(true);
+
+        groupLayout.setHorizontalGroup(groupLayout.createParallelGroup().addComponent(scrollPane));
+        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup().addComponent(scrollPane));
+        pack();
     }
 }
